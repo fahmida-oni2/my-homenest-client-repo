7ee17f4a5d2e22854ffe5d8df16ac4417184df60
fahmida-oni2 <onifahmida@gmail.com>
2025-11-09 23:54:50 +0600

    added the banner section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
+import Banner from '../../Components/Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-              Home            
+              <Banner></Banner>          
         </div>
     );
 };
